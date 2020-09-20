f458b5b82dde6c8210b5b5a18b67640ba4a0df68
Markus Leemet <markusleemet@gmail.com>
2020-09-20 11:12:16 +0300

    Fix error with ChromeDriver

--- src/main/java/cs/ut/Main.java
+++ src/main/java/cs/ut/Main.java
@@ -29,7 +29,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // Setup RemoteWebDrivers to run Selenium tests
         WebDriverManager.firefoxdriver().setup();
-        WebDriverManager.firefoxdriver().setup();
+        WebDriverManager.chromedriver().setup();
 
         runAllTests();
     }
